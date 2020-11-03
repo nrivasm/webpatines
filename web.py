6d3123e792c507e9123beae35dec4fc72e7d81a8
Nicolás de Rivas <65292155+nrivasm@users.noreply.github.com>
2020-11-03 22:20:03 +0100

    Create web.py

---
+++ web.py
@@ -0,0 +1,12 @@
+ #Grupo pablo
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+
+app = Flask(__name__)
+app.secret_key = "webchicles"
+
+@app.route("/")
+def home():
+    return render_template("index.html", page="home")
+
+if __name__ == "__main__":
+    app.run(debug=True)
