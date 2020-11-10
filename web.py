0a29a3388b2105a5d6cb8d8a413a931167874251
Nicolás de Rivas <65292155+nrivasm@users.noreply.github.com>
2020-11-10 07:19:13 +0100

    Add files via upload

--- web.py
+++ web.py
@@ -1,11 +1,28 @@
+ #Grupo pablo
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 
 app = Flask(__name__)
-app.secret_key = "webpatines"
+app.secret_key = "webchicles"
 
 @app.route("/")
 def home():
+    return render_template("index.html")
+
+@app.route("/App/")
+def aplicación():
     return render_template("index.html", page="home")
+
+@app.route("/Tarifas/")
+def tarifas():
+    return render_template("index.html", page="Tarifas")
+
+@app.route("/Terminos/")
+def terminos():
+    return render_template("index.html", page="Terminos")
+
+@app.route("/Contacto/")
+def contacto():
+    return render_template("contacto.html", page="Contacto")
 
 if __name__ == "__main__":
     app.run(debug=True)
